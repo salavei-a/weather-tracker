f054736124d0ef195453ceca41cef1f6545f78aa
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-10-07 19:02:01 +0400

    feat: add `HibernateContextListener`

---
+++ src/main/java/com/asalavei/weathertracker/web/listener/HibernateContextListener.java
@@ -0,0 +1,41 @@
+package com.asalavei.weathertracker.web.listener;
+
+import ch.qos.logback.classic.Level;
+import com.asalavei.weathertracker.dbaccess.config.HibernateConfig;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+@Slf4j
+@WebListener
+public class HibernateContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        HibernateConfig.getSessionFactory();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+
+        while (drivers.hasMoreElements()) {
+            Driver driver = drivers.nextElement();
+
+            try {
+                DriverManager.deregisterDriver(driver);
+                log.info("JDBC Driver deregistered successfully: {}", driver);
+            } catch (SQLException e) {
+                log.error("Failed to deregister JDBC Driver: {}", driver, e);
+            }
+        }
+
+        HibernateConfig.shutdown();
+    }
+}
