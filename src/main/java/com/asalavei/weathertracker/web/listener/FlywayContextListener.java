454008c6eaea1dd436f2600359a735508dd634e2
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-10-07 19:04:21 +0400

    feat: add `FlywayContextListener`

---
+++ src/main/java/com/asalavei/weathertracker/web/listener/FlywayContextListener.java
@@ -0,0 +1,23 @@
+package com.asalavei.weathertracker.web.listener;
+
+import com.asalavei.weathertracker.dbaccess.config.HibernateConfig;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.Flyway;
+
+@Slf4j
+@WebListener
+public class FlywayContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(HibernateConfig.getHikariDataSource())
+                .load();
+
+        flyway.migrate();
+        log.info("Flyway migration completed successfully");
+    }
+}
