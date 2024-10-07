257cf1d8ce9b3e785721b1fdf42b0102b9a2b069
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-10-07 15:12:06 +0400

    feat: add `V1__init_tables.sql`

---
+++ src/main/resources/db/migration/V1__init_tables.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id       BIGINT       NOT NULL GENERATED ALWAYS AS IDENTITY,
+    login    VARCHAR(39)  NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS locations
+(
+    id        BIGINT       NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name      VARCHAR(255) NOT NULL,
+    user_id   BIGINT       NOT NULL,
+    latitude  DECIMAL(9,6) NOT NULL,
+    longitude DECIMAL(9,6) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS sessions
+(
+    id         VARCHAR(36) NOT NULL,
+    user_id    BIGINT      NOT NULL,
+    expires_at TIMESTAMP   NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE INDEX idx_sessions_user_id ON sessions(user_id);
