de5b099497d95580b891a2c5d4984772672a1245
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-10-09 23:21:13 +0400

    feat: add `UserRequestDto` and `UserResponseDto`

---
+++ src/main/java/com/asalavei/weathertracker/dto/UserRequestDto.java
@@ -0,0 +1,11 @@
+package com.asalavei.weathertracker.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRequestDto {
+
+    private String username;
+
+    private String password;
+}
---
+++ src/main/java/com/asalavei/weathertracker/dto/UserResponseDto.java
@@ -0,0 +1,8 @@
+package com.asalavei.weathertracker.dto;
+
+import lombok.Value;
+
+@Value
+public class UserResponseDto {
+    String username;
+}
