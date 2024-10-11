a58ce95f7c82147627b752dc74f935f517228a0f
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-10-11 17:50:38 +0400

    feat: add new fields to `LocationRequestDto` and ignore unknown properties

--- src/main/java/com/asalavei/weathertracker/dto/LocationRequestDto.java
+++ src/main/java/com/asalavei/weathertracker/dto/LocationRequestDto.java
@@ -1,8 +1,17 @@
 package com.asalavei.weathertracker.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LocationRequestDto {
+
     private String name;
+
+    private BigDecimal latitude;
+
+    private BigDecimal longitude;
 }
