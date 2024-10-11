e6295291db284fe1764f34c62d8f7e25db0983d3
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-10-11 23:30:32 +0400

    feat: add state field to `LocationResponseDto`

--- src/main/java/com/asalavei/weathertracker/dto/LocationResponseDto.java
+++ src/main/java/com/asalavei/weathertracker/dto/LocationResponseDto.java
@@ -1,11 +1,13 @@
 package com.asalavei.weathertracker.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LocationResponseDto {
 
     private String name;
@@ -17,4 +19,6 @@ public class LocationResponseDto {
 
     @JsonProperty("lon")
     private BigDecimal longitude;
+
+    private String state;
 }
