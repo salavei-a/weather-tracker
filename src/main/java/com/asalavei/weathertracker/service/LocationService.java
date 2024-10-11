42718ec762edff0d7bf0a9398221900f11cb9ec4
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-10-11 17:51:03 +0400

    feat: add `LocationService`

---
+++ src/main/java/com/asalavei/weathertracker/service/LocationService.java
@@ -0,0 +1,34 @@
+package com.asalavei.weathertracker.service;
+
+import com.asalavei.weathertracker.dbaccess.entity.Location;
+import com.asalavei.weathertracker.dbaccess.entity.User;
+import com.asalavei.weathertracker.dbaccess.repository.LocationRepository;
+import com.asalavei.weathertracker.dto.LocationRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationService {
+
+    private final LocationRepository locationRepository;
+
+    @Autowired
+    public LocationService(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
+    }
+
+    public void create(LocationRequestDto locationRequest, User user) {
+        Location location = Location.builder()
+                .name(locationRequest.getName())
+                .user(user)
+                .latitude(locationRequest.getLatitude())
+                .longitude(locationRequest.getLongitude())
+                .build();
+
+        save(location);
+    }
+
+    private Location save(Location location) {
+        return locationRepository.save(location);
+    }
+}
