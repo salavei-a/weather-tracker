ea0ba1ce2893ac3196f453f339b0706336163123
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-10-11 17:57:17 +0400

    feat: add POST method to `LocationController` and update `locations.html`

--- src/main/java/com/asalavei/weathertracker/web/controller/LocationController.java
+++ src/main/java/com/asalavei/weathertracker/web/controller/LocationController.java
@@ -4,6 +4,7 @@ import com.asalavei.weathertracker.dbaccess.entity.User;
 import com.asalavei.weathertracker.dto.LocationRequestDto;
 import com.asalavei.weathertracker.dto.LocationResponseDto;
 import com.asalavei.weathertracker.mapper.UserMapper;
+import com.asalavei.weathertracker.service.LocationService;
 import com.asalavei.weathertracker.service.WeatherService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,16 +22,18 @@ import java.util.List;
 public class LocationController {
 
     private final WeatherService weatherService;
+    private final LocationService locationService;
     private final UserMapper userMapper;
 
     @Autowired
-    public LocationController(WeatherService weatherService, UserMapper userMapper) {
+    public LocationController(WeatherService weatherService, LocationService locationService, UserMapper userMapper) {
         this.weatherService = weatherService;
+        this.locationService = locationService;
         this.userMapper = userMapper;
     }
 
     @GetMapping
-    public String search(@ModelAttribute("location") LocationRequestDto locationRequestDto, HttpServletRequest req, Model model) {
+    public String search(@ModelAttribute("location") LocationRequestDto location, HttpServletRequest req, Model model) {
         User user = (User) req.getAttribute("authenticatedUser");
 
         if (user == null) {
@@ -38,14 +42,27 @@ public class LocationController {
 
         model.addAttribute("user", userMapper.toDto(user));
 
-        if (locationRequestDto.getName() == null) {
+        if (location.getName() == null) {
             return "locations";
         }
 
-        List<LocationResponseDto> locations = weatherService.getLocationDetails(locationRequestDto.getName());
+        List<LocationResponseDto> locations = weatherService.getLocationDetails(location.getName());
 
         model.addAttribute("locations", locations);
 
         return "locations";
+    }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute("location") LocationRequestDto locationRequest, HttpServletRequest req) {
+        User user = (User) req.getAttribute("authenticatedUser");
+
+        if (user == null) {
+            return "redirect:/auth/signin";
+        }
+
+        locationService.create(locationRequest, user);
+
+        return "redirect:/";
     }
 }
