7eb011a6d649122c3b415c09a8a1505e023f0be2
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-10-15 16:35:49 +0400

    feat: add `AppRuntimeException` and `WeatherServiceException`

---
+++ src/main/java/com/asalavei/weathertracker/exception/AppRuntimeException.java
@@ -0,0 +1,8 @@
+package com.asalavei.weathertracker.exception;
+
+public class AppRuntimeException extends RuntimeException {
+
+    public AppRuntimeException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/asalavei/weathertracker/exception/WeatherServiceException.java
@@ -0,0 +1,8 @@
+package com.asalavei.weathertracker.exception;
+
+public class WeatherServiceException extends AppRuntimeException {
+
+    public WeatherServiceException(String message) {
+        super(message);
+    }
+}
