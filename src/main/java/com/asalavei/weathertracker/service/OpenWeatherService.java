25ed4891f65bbf7d61fce5b5c2cab2db03f3022b
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-10-15 16:37:49 +0400

    feat: add logging of request and response bodies in `OpenWeatherService`

--- src/main/java/com/asalavei/weathertracker/service/OpenWeatherService.java
+++ src/main/java/com/asalavei/weathertracker/service/OpenWeatherService.java
@@ -3,23 +3,36 @@ package com.asalavei.weathertracker.service;
 import com.asalavei.weathertracker.entity.Location;
 import com.asalavei.weathertracker.dto.CurrentWeatherDto;
 import com.asalavei.weathertracker.dto.LocationResponseDto;
+import com.asalavei.weathertracker.exception.WeatherServiceException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class OpenWeatherService implements WeatherService {
 
     private static final String API_KEY = System.getenv("OPEN_WEATHER_API_KEY");
     private static final String WEATHER_API_PATH = "/data/2.5/weather";
     private static final String DIRECT_GEOCODING_API_PATH = "/geo/1.0/direct";
-    public static final String UNITS_OF_MEASUREMENT = "metric";
+    private static final String UNITS_OF_MEASUREMENT = "metric";
+
+    private static final String CLIENT_ERROR = "Client error occurred";
+    private static final String SERVER_ERROR = "Server error occurred";
+    private static final String CLIENT_ERROR_MESSAGE = "Failed to fetch weather data due to client error";
+    private static final String SERVER_ERROR_MESSAGE = "Failed to fetch weather data due to server error";
 
     private final RestClient restClient;
     private final LocationService locationService;
@@ -30,6 +43,7 @@ public class OpenWeatherService implements WeatherService {
         this.locationService = locationService;
     }
 
+    @Override
     public List<CurrentWeatherDto> getUserLocationsWeather(Long userId) {
         List<Location> userLocations = locationService.findAllByUserId(userId);
 
@@ -43,6 +57,7 @@ public class OpenWeatherService implements WeatherService {
                 .toList();
     }
 
+    @Override
     public List<LocationResponseDto> fetchLocationDetails(String locationName) {
         return restClient.get()
                 .uri(uriBuilder -> uriBuilder
@@ -52,9 +67,20 @@ public class OpenWeatherService implements WeatherService {
                         .queryParam("appid", API_KEY)
                         .build())
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                    logRequest(req);
+                    logResponseBody(CLIENT_ERROR, res);
+                    throw new WeatherServiceException(CLIENT_ERROR_MESSAGE);
+                })
+                .onStatus(HttpStatusCode::is5xxServerError, (req, res) -> {
+                    logRequest(req);
+                    logResponseBody(SERVER_ERROR, res);
+                    throw new WeatherServiceException(SERVER_ERROR_MESSAGE);
+                })
                 .body(new ParameterizedTypeReference<>() {});
     }
 
+    @Override
     public CurrentWeatherDto fetchWeatherByCoordinates(BigDecimal latitude, BigDecimal longitude) {
         return restClient.get()
                 .uri(uriBuilder -> uriBuilder
@@ -65,7 +91,26 @@ public class OpenWeatherService implements WeatherService {
                         .queryParam("units", UNITS_OF_MEASUREMENT)
                         .build())
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                    logRequest(req);
+                    logResponseBody(CLIENT_ERROR, res);
+                    throw new WeatherServiceException(CLIENT_ERROR_MESSAGE);
+                })
+                .onStatus(HttpStatusCode::is5xxServerError, (req, res) -> {
+                    logRequest(req);
+                    logResponseBody(SERVER_ERROR, res);
+                    throw new WeatherServiceException(SERVER_ERROR_MESSAGE);
+                })
                 .body(CurrentWeatherDto.class);
+    }
+
+    private void logRequest(HttpRequest req) {
+        log.error("Request failed. Method: {}, URI: {}", req.getMethod(), req.getURI());
+    }
+
+    private void logResponseBody(String errorType, ClientHttpResponse res) throws IOException {
+        String responseBody = new String(res.getBody().readAllBytes(), StandardCharsets.UTF_8);
+        log.error("{}. Status code: {}, Response body: {}", errorType, res.getStatusCode(), responseBody);
     }
 
     private CurrentWeatherDto roundTemperatures(CurrentWeatherDto currentWeather) {
