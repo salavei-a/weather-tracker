2e1c3ff95a29502beb493f176691f7fd1e761983
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-10-18 11:59:50 +0400

    refactor: remove redundant path in interceptor config

--- src/main/java/com/asalavei/weathertracker/config/WebConfig.java
+++ src/main/java/com/asalavei/weathertracker/config/WebConfig.java
@@ -33,7 +33,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor)
-                .excludePathPatterns("/auth/**", "/error/**");
+                .excludePathPatterns("/auth/**");
     }
 
     @Bean
