f9123a63c9533885b1603f598d0c3da9ed9a5123
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-10-18 17:16:35 +0400

    feat: handle `DatabaseOperationException`, `AlreadyExistsException`, and `AuthenticationException` in `GlobalExceptionHandler`

--- src/main/java/com/asalavei/weathertracker/handler/GlobalExceptionHandler.java
+++ src/main/java/com/asalavei/weathertracker/handler/GlobalExceptionHandler.java
@@ -1,23 +1,54 @@
 package com.asalavei.weathertracker.handler;
 
-import jakarta.servlet.http.HttpServletResponse;
+import com.asalavei.weathertracker.dto.UserRequestDto;
+import com.asalavei.weathertracker.exception.AlreadyExistsException;
+import com.asalavei.weathertracker.exception.AuthenticationException;
+import com.asalavei.weathertracker.exception.DatabaseOperationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.ui.Model;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public String handleNoHandlerFoundException(HttpServletResponse response) {
-        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNoHandlerFoundException() {
         return "error/404";
 
     }
 
     @ExceptionHandler(Exception.class)
-    public String handleException(HttpServletResponse response) {
-        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(Exception e) {
+        log.error("Unexpected error occurred", e);
         return "error/500";
+    }
+
+    @ExceptionHandler(DatabaseOperationException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleDatabaseOperationException() {
+        return "error/500";
+    }
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String handleAlreadyExistsException(AlreadyExistsException e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        model.addAttribute("user", new UserRequestDto());
+        return "auth/signup";
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String handleAuthenticationException(AuthenticationException e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        model.addAttribute("user", new UserRequestDto());
+        return "auth/signin";
     }
 }
