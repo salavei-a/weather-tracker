4dc9510606f9625288cdd72bfd567057d2c7171a
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-10-24 22:23:15 +0400

    feat: add Builder to `UserRequestDto`

--- src/main/java/com/asalavei/weathertracker/dto/UserRequestDto.java
+++ src/main/java/com/asalavei/weathertracker/dto/UserRequestDto.java
@@ -3,8 +3,14 @@ package com.asalavei.weathertracker.dto;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Data
 public class UserRequestDto {
 
