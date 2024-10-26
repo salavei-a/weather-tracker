c34dc7be6e3bbb4382bb153230ba2ca65bb38307
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-10-26 17:36:24 +0400

    fix: return `@NotBlank` for `UserRequestDto`

--- src/main/java/com/asalavei/weathertracker/dto/UserRequestDto.java
+++ src/main/java/com/asalavei/weathertracker/dto/UserRequestDto.java
@@ -1,6 +1,6 @@
 package com.asalavei.weathertracker.dto;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -16,11 +16,11 @@ public class UserRequestDto {
 
     @Pattern(regexp = "^\\s*[_.@A-Za-z0-9-]*\\s*$",
              message = "Username can only contain letters, digits, dots (.), underscores (_), at signs (@), and hyphens (-)")
-    @NotEmpty(message = "Username is required")
+    @NotBlank(message = "Username is required")
     @Size(min = 1, max = 39, message = "Username must be between 1 and 39 characters long ")
     private String username;
 
-    @NotEmpty(message = "Password is required")
+    @NotBlank(message = "Password is required")
     @Size(min = 1, max = 50, message = "Password must be between 1 and 50 characters long")
     private String password;
 }
